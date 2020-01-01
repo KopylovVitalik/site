c627717b7e146da65c5acb239b26dd0e0b313291
KopylovVitalik <vitalik.kopylov@gmail.com>
2020-01-01 22:35:36 +0200

    added reach text

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
+import MenuLink from "../components/menu-link"
+
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+const Bold = ({ children }) => <span className="bold">{children}</span>
+const Text = ({ children }) => <p className="align-center">{children}</p>
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: text => <Bold>{text}</Bold>,
+  },
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+  },
+}
 
 export const query = graphql`
   query($slug: String!) {
@@ -7,44 +23,53 @@ export const query = graphql`
       name
       slug
       tag
+      href
       image {
         fluid {
           src
         }
       }
       childContentfulProjectDescriptionRichTextNode {
-        content {
-          content {
-            value
-          }
-        }
+        json
       }
     }
   }
 `
 
-const Project = props => (
-  <>
-  <section className="hero is-success is-fullheight-with-navbar is-bold">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns">
-            <div className="is-one-third">
-              <img src={props.data.contentfulProject.image.fluid.src} alt=""/>
-            </div>
-            <div className="two-thirds">
-              <h1 className="title">{props.data.contentfulProject.name}</h1>
-              <p dangerouslySetInnerHTML={{
-            __html: props.data.contentfulProject.childContentfulProjectDescriptionRichTextNode.content.value}}>
-               
-              </p>
+const Project = props => {
+  const { contentfulProject: project } = props.data;
+  return (
+    <>
+      <section className="hero is-success is-fullheight-with-navbar is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns">
+              <div className="is-one-third column">
+                <img src={props.data.contentfulProject.image.fluid.src} alt="" />
+              </div>
+              <div className="two-thirds column">
+                <h1 className="title">{project.name}</h1>
+                {props.data.contentfulProject
+                  .childContentfulProjectDescriptionRichTextNode.json &&
+                  documentToReactComponents(
+                    props.data.contentfulProject
+                      .childContentfulProjectDescriptionRichTextNode.json,
+                    options
+                  )}
+                <a href={project.href} className="button is-link is-light" target="_blank">Link to project</a>
+                <MenuLink
+                  className="button is-danger is-outlined"
+                  to="/projects/"
+                  direction="up"
+                >
+                  To all projects
+              </MenuLink>
+              </div>
             </div>
           </div>
-          
         </div>
-      </div>
-    </section>
-  </>
-)
+      </section>
+    </>)
+}
 
 export default Project
